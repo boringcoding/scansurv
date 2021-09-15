66b69025af9359e7600a61f43ebee61311eaf301
Gregory Boring <boringcoding.com@gmail.com>
2021-09-15 21:14:52 +0800

    fix form upload

--- site/components/forms/dropzone.js
+++ site/components/forms/dropzone.js
@@ -9,115 +9,116 @@ const DropzoneWrapper = styled.div`
   ${props => [
     // console.log(props),
     props.isDragAccept
-      ? tw`border-success`
-      : props.isDragReject
-      ? tw`border-error`
-      : props.isDragActive
-      ? tw`border-primary`
-      : null,
+            ? tw`border-success`
+            : props.isDragReject
+                    ? tw`border-error`
+                    : props.isDragActive
+                            ? tw`border-primary`
+                            : null,
   ]}
 `
 
 const filereader = file => {
-  const reader = new FileReader()
-
-  reader.onabort = () => console.log("file reading was aborted")
-  reader.onerror = () => console.log("file reading has failed")
-  reader.onload = () => {
-    // Do whatever you want with the file contents
-    const binaryStr = reader.result
-    console.log(binaryStr)
-  }
-  reader.readAsArrayBuffer(file)
+	const reader = new FileReader()
+	
+	reader.onabort = () => console.log("file reading was aborted")
+	reader.onerror = () => console.log("file reading has failed")
+	reader.onload = () => {
+		// Do whatever you want with the file contents
+		file.src = reader.result
+	}
+	// reader.readAsArrayBuffer(file)
+	reader.readAsDataURL(file)
 }
 
 const Dropzone = rhfProps => {
-  const {
-    acceptedFiles,
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-  } = useDropzone({
-    // accept: "image/*",
-    multiple: true,
-    onDrop: acceptedFiles => {
-      setFiles(
-        acceptedFiles.map(file =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-            fileReader: filereader(file),
-          })
-        )
-      )
-    },
-    maxFiles: 5,
-    maxSize: 10485760,
-  })
-
-  const [files, setFiles] = useState([])
-
-  const thumbs = files.map((file, i) => (
-    <div
-      tw="inline-flex rounded border mb-3 mr-3 w-full h-full p-3"
-      key={file.name}
-    >
-      <div tw="flex items-center justify-between w-full overflow-hidden relative">
-        <div>
-          <p tw="my-0">{file.name}</p>
-          <p tw="text-sm font-bold my-0">{bytesToSize(file.size)}</p>
-          {/^image/.test(file.type) && (
-            <img
-              src={file.preview}
-              tw="width[100px] height[100px] object-cover"
-            />
-          )}
-        </div>
-        <span
-          onClick={() => remove(i)}
-          tw="text-error bg-white transition-colors duration-150 shadow hocus:(bg-error text-white) text-sm p-1 border border-error font-bold cursor-pointer"
-        >
+	const {
+		acceptedFiles,
+		getRootProps,
+		getInputProps,
+		isDragActive,
+		isDragAccept,
+		isDragReject,
+	} = useDropzone({
+		// accept: "image/*",
+		multiple: true,
+		onDrop: acceptedFiles => {
+			console.log(acceptedFiles)
+			setFiles(
+				acceptedFiles.map(file =>
+					Object.assign(file, {
+						preview: URL.createObjectURL(file),
+						fileReader: filereader(file),
+					})
+				)
+			)
+		},
+		maxFiles: 5,
+		maxSize: 10485760,
+	})
+	
+	const [files, setFiles] = useState([])
+	
+	const thumbs = files.map((file, i) => (
+		<div
+			tw="inline-flex rounded border mb-3 mr-3 w-full h-full p-3"
+			key={file.name}
+		>
+			<div tw="flex items-center justify-between w-full overflow-hidden relative">
+				<div>
+					<p tw="my-0">{file.name}</p>
+					<p tw="text-sm font-bold my-0">{bytesToSize(file.size)}</p>
+					{/^image/.test(file.type) && (
+						<img
+							src={file.preview}
+							tw="width[100px] height[100px] object-cover"
+						/>
+					)}
+				</div>
+				<span
+					onClick={() => remove(i)}
+					tw="text-error bg-white transition-colors duration-150 shadow hocus:(bg-error text-white) text-sm p-1 border border-error font-bold cursor-pointer"
+				>
           REMOVE
         </span>
-      </div>
-    </div>
-  ))
-
-  const remove = file => {
-    const newFiles = [...files]
-    newFiles.splice(file, 1)
-    setFiles(newFiles)
-  }
-
-  useEffect(
-    () => () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      files.forEach(file => URL.revokeObjectURL(file.preview))
-    },
-    [files]
-  )
-
-  return (
-    <>
-      <DropzoneWrapper
-        {...getRootProps({ isDragActive, isDragAccept, isDragReject })}
-        value={rhfProps.field.value ? rhfProps.field.value : ""}
-        onChange={e => rhfProps.field.onChange(e.target.files)}
-      >
-        <input {...getInputProps()} />
-        <p tw="mb-0">
-          Drag &lsquo;n&rsquo; drop some files here, or click to select files
-        </p>
-        <p tw="text-gray-400 text-sm mt-0">
-          Max files allowed is 5. Max upload size is 10mb.
-        </p>
-      </DropzoneWrapper>
-      <aside>
-        <ul>{thumbs}</ul>
-      </aside>
-    </>
-  )
+			</div>
+		</div>
+	))
+	
+	const remove = file => {
+		const newFiles = [...files]
+		newFiles.splice(file, 1)
+		setFiles(newFiles)
+	}
+	
+	useEffect(
+		() => () => {
+			// Make sure to revoke the data uris to avoid memory leaks
+			files.forEach(file => URL.revokeObjectURL(file.preview))
+		},
+		[files]
+	)
+	
+	return (
+		<>
+			<DropzoneWrapper
+				{...getRootProps({ isDragActive, isDragAccept, isDragReject })}
+				value={rhfProps.field.value ? rhfProps.field.value : ""}
+				onChange={e => rhfProps.field.onChange(e.target.files)}
+			>
+				<input {...getInputProps()} />
+				<p tw="mb-0">
+					Drag &lsquo;n&rsquo; drop some files here, or click to select files
+				</p>
+				<p tw="text-gray-400 text-sm mt-0">
+					Max files allowed is 5. Max upload size is 10mb.
+				</p>
+			</DropzoneWrapper>
+			<aside>
+				<ul>{thumbs}</ul>
+			</aside>
+		</>
+	)
 }
 
 export default Dropzone
--- site/pages/api/recaptcha.js
+++ site/pages/api/recaptcha.js
@@ -3,100 +3,109 @@ import fs from "fs"
 import { camelToCapSentence } from "@/utils/helpers"
 
 export default async function handler(req, res) {
-  const { body, method } = req
-
-  // Extract the formData, formName and captcha code from the request body
-  const { formData, formName, captcha } = body
-
-  if (method === "POST") {
-    if (!formData || !captcha) {
-      return res.status(422).json({
-        message: "Unproccesable request, please provide the required fields",
-      })
-    }
-
-    try {
-      const response = await fetch(
-        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-          },
-          method: "POST",
-        }
-      )
-      const captchaValidation = await response.json()
-      if (captchaValidation.success) {
-        const nodemailer = require("nodemailer")
-        const mg = require("nodemailer-mailgun-transport")
-
-        var htmlBody = ""
-        for (const [key, value] of Object.entries(formData.form)) {
-          if (key !== "files") {
-            htmlBody += `<p><b>${camelToCapSentence(key)}: </b>${value}</p>`
-          }
-        }
-
-        /**
-         * Initialise Mailgun transporter
-         */
-        const nodemailerMailgun = nodemailer.createTransport(
-          mg({
-            auth: {
-              api_key: process.env.MAILGUN_API_KEY,
-              domain: process.env.MAILGUN_DOMAIN,
-            },
-            host: process.env.MAILGUN_HOST,
-          })
-        )
-
-        /**
-         * If any attachments, add them
-         */
-        // let attachments = []
-        // if (formData.form?.files) {
-        //   let buffer = fs.readFileSync(formData.form.files[0].path)
-        //   attachments.push(fs.createReadStream(buffer))
-        // }
-        // console.log(attachments)
-        console.log(formData.form.files[0])
-
-        /**
-         * Send the email
-         */
-        // await new Promise((resolve, reject) => {
-        //   nodemailerMailgun.sendMail(
-        //     {
-        //       from: process.env.MAILGUN_EMAIL_FROM,
-        //       to: process.env.MAILGUN_EMAIL_TO,
-        //       subject: "New form submission",
-        //       html: htmlBody,
-        //       attachments: attachments,
-        //     },
-        //     (err, info) => {
-        //       if (err) {
-        //         console.log(`Error: ${err}`)
-        //         reject(err)
-        //       } else {
-        //         console.log(`Response: ${info}`)
-        //         resolve(info)
-        //       }
-        //     }
-        //   )
-        // })
-
-        // Return 200 if everything is successful
-        return res.status(200).send("OK")
-      }
-
-      return res.status(422).json({
-        message: "Unproccesable request, Invalid captcha code",
-      })
-    } catch (error) {
-      console.log(error)
-      return res.status(422).json({ message: "Something went wrong" })
-    }
-  }
-  // Return 404 if someone pings the API with a method other than POST
-  return res.status(404).send("Not found")
+	const { body, method } = req
+	
+	// Extract the formData, formName and captcha code from the request body
+	const { formData, formName, captcha } = body
+	console.log(formData, captcha)
+	if (method === "POST") {
+		if (!formData || !captcha) {
+			return res.status(422).json({
+				message: "Unproccesable request, please provide the required fields",
+			})
+		}
+		
+		try {
+			const response = await fetch(
+				`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
+				{
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+					},
+					method: "POST",
+				}
+			)
+			const captchaValidation = await response.json()
+			if (captchaValidation.success) {
+				const nodemailer = require("nodemailer")
+				const mg = require("nodemailer-mailgun-transport")
+				
+				var htmlBody = ""
+				for (const [key, value] of Object.entries(formData.form)) {
+					if (key !== "files") {
+						htmlBody += `<p><b>${camelToCapSentence(key)}: </b>${value}</p>`
+					}
+				}
+				
+				/**
+				 * Initialise Mailgun transporter
+				 */
+				const nodemailerMailgun = nodemailer.createTransport(
+					mg({
+						auth: {
+							api_key: process.env.MAILGUN_API_KEY,
+							domain: process.env.MAILGUN_DOMAIN,
+						},
+						host: process.env.MAILGUN_HOST,
+					})
+				)
+				
+				//
+				/**
+				 * If any attachments, add them
+				 */
+				let attachments = []
+				if (formData.form?.files) {
+					const files = Object.entries(formData.form.files).map(file => {
+						return file[1]
+					})
+					files.forEach(file => {
+						attachments.push({
+							path: file.src,
+						})
+					})
+				}
+				
+				console.log(attachments)
+				
+				// console.log(formData.form.files[0].src)
+				
+				/**
+				 * Send the email
+				 */
+				await new Promise((resolve, reject) => {
+					nodemailerMailgun.sendMail(
+						{
+							from: process.env.MAILGUN_EMAIL_FROM,
+							to: process.env.MAILGUN_EMAIL_TO,
+							subject: "New form submission",
+							html: htmlBody,
+							attachments: attachments,
+						},
+						(err, info) => {
+							if (err) {
+								console.log(`Error: ${err}`)
+								reject(err)
+							} else {
+								console.log(`Response: ${info}`)
+								resolve(info)
+							}
+						}
+					)
+				})
+				
+				// Return 200 if everything is successful
+				return res.status(200).send("OK")
+			}
+			
+			return res.status(422).json({
+				message: "Unproccesable request, Invalid captcha code",
+			})
+		} catch (error) {
+			console.log(error)
+			return res.status(422).json({ message: "Something went wrong" })
+		}
+	}
+	// Return 404 if someone pings the API with a method other than POST
+	return res.status(404).send("Not found")
 }
